0e647a748edee433fde6302f7aebc5e79df1d48a
Jelle Roets <jelleroets@outlook.be>
2019-02-15 18:27:38 +0100

    let proxy use config object from server repo

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,10 @@
 const proxy = require('http-proxy-middleware');
-const server = process.env.SERVER === "local" ? 'http://localhost:8080/' : 'http://raspberrypi.local:8080/';
+let hostName = 'raspberrypi.local';
+try {
+  config = require('../../server/config/config');
+  hostName = config.hostName;
+} catch {} 
+const server = process.env.SERVER === "local" ? 'http://localhost:8080/' : `http://${hostName}:8080/`;
 
 module.exports = function(app) {
   app.use(proxy('/API', { target: server}));
